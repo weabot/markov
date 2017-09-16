bdd8106a3103d6984fbc4909bd39bff11bcfc835
weabot <weabot@yandex.ru>
2017-09-16 00:52:21 -0400

    Removed magic newline handling for now

--- markov.pl
+++ markov.pl
@@ -1,6 +1,6 @@
-sub MAIN($file, $order = 2, $maxwords = 75, $finalword = "".Str, $ignorenl=False) {
+sub MAIN($file, $order = 2, $maxwords = 75, $finalword = "") {
 	my %table = markov_read($file, $order);
-	my $output = markov_gen(%table, $order, $maxwords, $finalword, $ignorenl);
+	my $output = markov_gen(%table, $order, $maxwords, $finalword);
 	put($output);
 	exit(0);
 }
@@ -13,18 +13,12 @@ sub markov_read($file, $order) {
 	my @prefix;
 	my %table;
 
-	# Fill an array with empty strings to quickly reset the prefix
-	my @empty_array;
 	loop (my $i = 0; $i < $order; $i++) {
-		@empty_array[$i] = $empty;
+		@prefix[$i] = $empty;
 	}
 
 	for @input -> $line {
-		@prefix = @empty_array;
-
-		# Keep a final empty string to signify line breaks
 		my @words = $line.words;
-		@words.append($empty);
 
 		# Add the entire line
 		for @words -> $word {
@@ -37,16 +31,18 @@ sub markov_read($file, $order) {
 			@prefix.shift;
 			@prefix[$order - 1] = $word;
 		}
+
 	}
 
 	return %table;
 }
 
 # Generate a string based on the given table.
-sub markov_gen(%table, $order, $maxwords, $finalword, $ignorenl) {
+sub markov_gen(%table, $order, $maxwords, $finalword) {
 	my @prefix = [];
 	my @output = [];
 	my $empty = "".Str;
+
 	# Fill the prefix with empty entries to get a first word in the string.
 	loop (my $i = 0; $i < $order; $i++) {
 		@prefix[$i] = $empty;
@@ -54,20 +50,17 @@ sub markov_gen(%table, $order, $maxwords, $finalword, $ignorenl) {
 
 	my $wordc = 0;
 	my $index = %table{@prefix.join}.elems.rand.Int;
-	my $nextword = %table{@prefix.join}[$index];
-	while ($nextword !eq $finalword && $wordc < $maxwords) { 
-		if ($nextword !eq $empty) {
-			@output.append($nextword);
-		} elsif (!$ignorenl) {
-			@output.append("\n");
-		}
-
-		@prefix.shift;
-		@prefix[$order - 1] = $nextword;
-
+	my $word = %table{@prefix.join}[$index];
+	while ($word !eq $finalword && $wordc < $maxwords) { 
 		$index = %table{@prefix.join}.elems.rand.Int;
-		$nextword = %table{@prefix.join}[$index];
-		$wordc++;
+		$word = %table{@prefix.join}[$index];
+
+		if ($word !eq $empty) {
+			@output.append($word);
+			@prefix.shift;
+			@prefix[$order - 1] = $word;
+			$wordc++;
+		}
 	}
 
 	return @output.join(" ");
